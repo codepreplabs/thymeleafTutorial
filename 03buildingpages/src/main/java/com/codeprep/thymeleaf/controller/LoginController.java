1b07f6f6ea70c06051a9fd8ab0dec6c10f83a033
praveen <thejavamonk@gmail.com>
2020-12-11 15:23:52 +0530

    example on building pages using fragments, creating local variables, adding dynamic styles

---
+++ 03buildingpages/src/main/java/com/codeprep/thymeleaf/controller/LoginController.java
@@ -0,0 +1,35 @@
+package com.codeprep.thymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping("/")
+    public ModelAndView homePage(){
+        Map<String, Object> model = new HashMap<>();
+        model.put("username","user1");
+        model.put("id","10");
+        return new ModelAndView("home", "model", model);
+    }
+
+    @RequestMapping("/profile")
+    public ModelAndView viewProfile(){
+        Map<String, Object> model = new HashMap<>();
+        model.put("title", "Mr");
+        model.put("firstName", "UserFN");
+        model.put("surName", "UserLN");
+        model.put("dateOfBirth", new GregorianCalendar(2020, 12, 7).getTime());
+        model.put("description", "a <strong> fantastic </strong> Java Programmer");
+        model.put("languages", List.of("Java","JavaScript","Unix Shell Programming","Groovy"));
+        model.put("color","#ffa833");
+        return new ModelAndView("profile", "model", model);
+    }
+}
